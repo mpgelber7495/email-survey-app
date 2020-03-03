ec5c7938b0163b2df2b111e5f768c19dca0f10c9
Michael Gelber <mpgelber7495@gmail.com>
2020-03-03 16:08:17 -0500

    Feature: Authentication complete but req.user is undefined

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,19 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+// put the info into the cookie
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user.id);
+});
+
+// remove the info from the cookie
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,7 +26,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user for this googleId - create a new user
+          new User({ googleId: profile.id }).save().then(user => {
+            done(null, user);
+          });
+        }
+      });
     }
   )
 );
