9c1a7f99cdf8f23f82b8bd9531123b329c6368c5
Michael Gelber <mpgelber7495@gmail.com>
2020-03-11 14:50:47 -0400

    Feature: Mongoose integrated update with webook from sendgrid

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -10,6 +13,28 @@ module.exports = app => {
   app.get("/api/surveys/thanks", (req, res) => {
     res.send("Thanks for voting!");
   });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+
+    const events = _chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .value();
+
+    res.send({});
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
